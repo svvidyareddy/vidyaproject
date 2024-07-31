021202f6542784531a7d71b2a7d839032b1c493d
svvidyareddy <165146836+svvidyareddy@users.noreply.github.com>
2024-07-30 23:32:41 -0700

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,8 @@
+#write a python code to read a,b,c and slove the equation s=a^3*b*c+c^5*b-a
+a=float(input("Enter value of a:"))
+b=float(input("Enter value of b:"))
+c=float(input("Enter value of c:"))
+
+s=a**3*b*c+c**5*b-a
+
+print("Equation of s is :",s)
